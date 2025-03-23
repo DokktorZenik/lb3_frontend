8b5eb90096d2217305789be6ab6c96addaabd872
Dan <yan-daniil.zinovieiev@nure.ua>
2025-03-23 15:33:00 +0200

    final

--- src/App.js
+++ src/App.js
@@ -4,50 +4,103 @@ import axios from "axios";
 
 function App() {
 
-  const [text, setText] = useState('');
+    const [form, setForm] = useState({
+        createdBy: "",
+        filmName: "",
+        reviewContent: ""
+    });
 
-  const toggleStart = () => {
-    axios.post("http://localhost:5276/start")
-        .then(res => {window.alert(res.data)})
-        .catch(err => console.log(err));
-  }
+    const [reviews, setReviews] = useState([])
 
-  const toggleCheck = () => {
-    axios.get("http://localhost:5276/status")
-        .then(res => {window.alert(res.data)})
-        .catch(err => console.log(err));
-  }
+    const toggleStart = () => {
+        axios.post("http://localhost:5276/start")
+            .then(res => {
+                window.alert(res.data)
+            })
+            .catch(err => console.log(err));
+    }
 
-  const toggleResult = () => {
-    axios.post("http://localhost:5276/predict", {
-      ReviewText: text
-    })
-        .then(res => {window.alert(res.data)})
-        .catch(err => console.log(err));
-  }
+    const toggleCheck = () => {
+        axios.get("http://localhost:5276/status")
+            .then(res => {
+                window.alert(res.data)
+            })
+            .catch(err => console.log(err));
+    }
 
-  return (
-    <div className="App">
-      <div className="form-wrapper">
-        <div className="form-container">
-          <div className="input-container">
-            <textarea  onChange={e  => setText(e.target.value)} value={text} />
-          </div>
-          <div className="button-wrapper">
-            <div className="button green" onClick={toggleStart}>
-              START
+    const toggleResult = () => {
+        axios.post("http://localhost:5276/predict", {...form, reviewDate: new Date().toISOString().split('T')[0]}
+        )
+            .then(res => {
+                window.alert(res.data)
+            })
+            .catch(err => console.log(err));
+    }
+
+    const toggleReviews = () => {
+        axios.get("http://localhost:5276/reviews")
+            .then(res => {
+                setReviews(res.data)
+            })
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <div className="App">
+            <div className="form-wrapper">
+                <div className="form-container">
+                    <div className="input-container">
+                        <p className="createdBy__label">Created By:</p>
+                        <input type="text" className="createdBy__input"
+                               onChange={e => setForm({...form, createdBy: e.target.value})} value={form.createdBy}/>
+                        <p>Film:</p>
+                        <input type="text" className="film" onChange={e => setForm({...form, filmName: e.target.value})}
+                               value={form.filmName}/>
+                        <p>Review:</p>
+                        <textarea onChange={e => setForm({...form, reviewContent: e.target.value})}
+                                  value={form.reviewContent}/>
+                    </div>
+                    <div className="button-wrapper">
+                        <div className="button green" onClick={toggleStart}>
+                            START
+                        </div>
+                        <div className="button check" onClick={toggleCheck}>
+                            CHECK
+                        </div>
+                        <div className="button green" onClick={toggleResult}>
+                            APPLY
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="button check" onClick={toggleCheck}>
-              CHECK
+            <div className="review-form">
+                <div className="reviews">
+                    {reviews.map(review => (
+                        <div className="review">
+                            <div className="date separator">
+                                {review.reviewDate}
+                            </div>
+                            <div className="person separator">
+                                {review.createdBy}
+                            </div>
+                            <div className="content">
+                                <p className="film_name">
+                                    {review.filmName}
+                                </p>
+                                <p className="review_content">
+                                    {review.reviewContent}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+                <div className="button green" onClick={toggleReviews}>
+                    UPDATE
+                </div>
             </div>
-            <div className="button green" onClick={toggleResult}>
-              APPLY
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
